bae309596ac94fe0c0ae5a5aa50d3e52abb9267d
tam-doan <tam.d@shopstack.asia>
2019-02-28 16:14:55 +0700

    Updates

---
+++ precache-manifest.b22798488385c8ace5fccf10dfae4c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tampub/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ff5db4f0e3355613c42",
+    "url": "/tampub/static/js/runtime~main.9ff5db4f.js"
+  },
+  {
+    "revision": "a1f1365c3c43e50a2c04",
+    "url": "/tampub/static/js/main.a1f1365c.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/tampub/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "a1f1365c3c43e50a2c04",
+    "url": "/tampub/static/css/main.6af0e1a2.chunk.css"
+  },
+  {
+    "revision": "1180a364b5b63c49c3e3fbb7c0fc85bf",
+    "url": "/tampub/index.html"
+  }
+];
